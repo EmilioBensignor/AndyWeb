3c2e9d8a6b4fa0f190dec1dbd56884654b8d472d
Emilio Bensignor <liobensignor@gmail.com>
2025-04-24 15:56:48 -0300

    Scroll reveal Frase

--- components/home/Frase.vue
+++ components/home/Frase.vue
@@ -1,13 +1,35 @@
 <template>
-    <DefaultSection
-        class="bg-[url('/images/backgrounds/Flores-Andy-Loisch.jpg')] bg-cover bg-center bg-no-repeat">
+    <DefaultSection class="bg-[url('/images/backgrounds/Flores-Andy-Loisch.jpg')] bg-cover bg-center bg-no-repeat">
         <div class="px-5 pt-[3.75rem] pb-[7.5rem]">
-            <DefaultContent>
+            <DefaultContent class="scroll-reveal">
                 <h2 class="text-xl tracking-widest text-light">
-                    ESTA ES UNA FRASE SOBRE
-                    INSPIRACION, ARTE Y VIDA COTIDIANA
+                    <span class="text-[hsl(0_0%_100%_/_0.2)] bg-clip-text">
+                        ESTA ES UNA FRASE SOBRE
+                        INSPIRACION, ARTE Y VIDA COTIDIANA
+                    </span>
                 </h2>
             </DefaultContent>
         </div>
     </DefaultSection>
 </template>
+
+<style scoped>
+.scroll-reveal span {
+    background-repeat: no-repeat;
+    background-size: 0% 100%;
+    background-image: linear-gradient(90deg, red, red);
+    animation: scroll-reveal linear forwards;
+    animation-timeline: view();
+}
+
+.scroll-reveal h2 span {
+    animation-range-start: contain 30vh;
+    animation-range-end: contain 60vh;
+}
+
+@keyframes scroll-reveal {
+    to {
+        background-size: 100% 100%;
+    }
+}
+</style>
