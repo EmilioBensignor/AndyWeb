2909285fe6f4f385e63e6ff0c3961b238f21cacd
Lio <liobensignor@gmail.com>
2025-04-23 21:17:56 -0300

    Grid de Obras + Superposicion Frase y Obras

--- components/home/Frase.vue
+++ components/home/Frase.vue
@@ -3,7 +3,7 @@
         class="bg-[url('/images/backgrounds/Flores-Andy-Loisch.jpg')] bg-cover bg-center bg-no-repeat">
         <div class="px-5 pt-[3.75rem] pb-[7.5rem]">
             <DefaultContent>
-                <h2 class="text-xl text-light">
+                <h2 class="text-xl tracking-widest text-light">
                     ESTA ES UNA FRASE SOBRE
                     INSPIRACION, ARTE Y VIDA COTIDIANA
                 </h2>
--- components/home/Obras.vue
+++ components/home/Obras.vue
@@ -1,12 +1,58 @@
 <template>
-    <DefaultSection>
-        <DefaultContent class="flex flex-col items-center pt-[7.5rem] px-5 pb-[3.75rem]">
-            <NuxtLink :to="ROUTE_NAMES.OBRAS" class=" bg-secondary rounded-full border border-light text-center text-light py-5 px-7">VER TODAS LAS OBRAS</NuxtLink>
+    <DefaultSection class="bg-gradient-to-b from-darkOpacity to-dark px-5 pt-[7.5rem] pb-[3.75rem] -mt-[10rem]">
+        <DefaultContent>
+            <div class="grid grid-cols-3 grid-rows-4 xl:grid-cols-4 xl:grid-rows-3 gap-2 w-full max-w-6xl mx-auto">
+                <div v-for="(img, index) in images" :key="index" :class="[baseGrid[index], responsiveGrid[index]]">
+                    <img :src="`/images/obras/${img}-Andy-Loisch.png`" :alt="`${img} Andy Loisch`"
+                        class="w-full h-full object-cover rounded-md shadow-md" />
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-10">
+                <NuxtLink :to="ROUTE_NAMES.OBRAS"
+                    class="bg-secondary rounded-full border border-light text-center text-light py-5 px-7">
+                    VER TODAS LAS OBRAS
+                </NuxtLink>
+            </div>
         </DefaultContent>
     </DefaultSection>
 </template>
 
 <script setup>
-import { ROUTE_NAMES } from '~/constants/ROUTE_NAMES';
+import { ROUTE_NAMES } from '~/constants/ROUTE_NAMES'
 
+const images = [
+    'Bordado',
+    'Bordados',
+    'Collage',
+    'Composicion',
+    'Intervenciones',
+    'Obras',
+    'Tejido',
+    'Textil',
+]
+
+// Base layout: grid de 3 columnas × 4 filas
+const baseGrid = [
+    'col-span-3 row-span-1', // div1
+    'col-span-1 row-span-1', // div2
+    'col-span-1 row-span-1', // div3
+    'col-span-1 row-span-1', // div4
+    'col-span-2 row-span-1', // div5
+    'col-span-1 row-span-1', // div6
+    'col-span-1 row-span-1', // div7
+    'col-span-2 row-span-1', // div8
+]
+
+// Responsive layout: grid de 4 columnas × 3 filas (a partir de xl)
+const responsiveGrid = [
+    'xl:col-span-2 xl:row-span-1', // div1
+    'xl:col-span-1 xl:row-span-1', // div2
+    'xl:col-span-1 xl:row-span-1', // div3
+    'xl:col-span-1 xl:row-span-1', // div4
+    'xl:col-span-2 xl:row-span-1', // div5
+    'xl:col-span-1 xl:row-span-1', // div6
+    'xl:col-span-2 xl:row-span-1', // div7
+    'xl:col-span-2 xl:row-span-1', // div8
+]
 </script>
