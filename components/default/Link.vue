219dee0a53e35c0f14e9e585a74828e537b66f52
Lio <liobensignor@gmail.com>
2025-04-30 12:25:07 -0300

    Link hover en desktop

--- components/default/Link.vue
+++ components/default/Link.vue
@@ -1,6 +1,6 @@
 <template>
     <NuxtLink
-        class="text-light font-medium text-sm md:text-lg xl:text-2xl 2xl:text-[1.75rem] relative before:content-[''] before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-0 before:z-[-1] before:bg-white before:transition-all before:duration-300 before:hover:w-full">
+        class="text-light font-medium text-sm md:text-lg xl:text-2xl 2xl:text-[1.75rem] relative before:content-[''] before:w-0 before:h-[2px] before:absolute before:left-0 before:bottom-0 before:z-[-1] before:bg-white before:transition-all before:duration-300 xl:before:hover:w-full">
         <slot />
     </NuxtLink>
 </template>
