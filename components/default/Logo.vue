25027fbe6f669700970000023b834f9962c31337
Lio <liobensignor@gmail.com>
2025-05-15 15:15:20 -0300

    Test logo width

--- components/default/Logo.vue
+++ components/default/Logo.vue
@@ -1,7 +1,7 @@
 <template>
     <NuxtLink :to="ROUTE_NAMES.HOME">
         <NuxtImg src="/images/Andy-Loisch-Logo.svg" alt="Andy Loisch Logo"
-            class="w-full h-8 md:h-12 xl:h-16 2xl:h-20 object-contain" />
+            class="h-8 md:h-12 xl:h-16 2xl:h-20 object-contain" />
     </NuxtLink>
 </template>
 
